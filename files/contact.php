8f74aad3f9dc49ce52d2abdd983bb5cf4b4b1e1b
MOTY <50269513+MOTY12@users.noreply.github.com>
2021-03-10 18:15:06 +0100

    Update contact.php

--- files/contact.php
+++ files/contact.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\Exception;
 //Load Composer's autoloader
 require '../vendor/autoload.php';
 
-if(isset($_POST["send"])) {
+if(isset($_POST["name"])) {
     $name = $_POST["name"];
 	$phonenumber = $_POST["phonenumber"];
     $email = $_POST["email"];
